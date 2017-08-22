ba458dc4febc223f5c5b1078f66ab030d22883c5
MisterKozo <31244997+MisterKozo@users.noreply.github.com>
2017-08-22 21:26:08 +0300

    Add missing file

---
+++ dicthandler.py
@@ -0,0 +1,23 @@
+def _SeparateAndRequest(dictionary, char):
+	words = []
+	i     = 0
+	text  = dictionary.split('\n')
+
+	for l in range(0,len(lines)):
+		line = text[l].split(' ')
+		if line[0] == char:
+			words[i] = line[1]
+			i++
+
+	return words
+
+class Dictionary:
+	def __init__(self, filename):
+		with open(filename, 'r') as dfile:
+			dictionary = dfile.read()
+	def LoadNouns:
+		return _SeparateAndRequest(dictionary, 'N')
+	def LoadVerbs:
+		return _SeparateAndRequest(dictionary, 'V')
+	def LoadCuss:
+		return _SeparateAndRequest(dictionary, 'C')
