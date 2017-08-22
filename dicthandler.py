65b9cd547dc0e983ea67df1291c6939e316e6daf
Ahmed <adhoms@adhoms.public.domain.not.really>
2017-08-22 21:59:30 +0300

    Solved scope issues

--- dicthandler.py
+++ dicthandler.py
@@ -3,21 +3,21 @@ def _SeparateAndRequest(dictionary, char):
 	i     = 0
 	text  = dictionary.split('\n')
 
-	for l in range(0,len(lines)):
+	for l in range(0,len(text)):
 		line = text[l].split(' ')
 		if line[0] == char:
 			words[i] = line[1]
-			i = i + 1
+			i += 1
 
 	return words
 
 class Dictionary:
 	def __init__(self, filename):
 		with open(filename, 'r') as dfile:
-			dictionary = dfile.read()
-	def LoadNouns():
-		return _SeparateAndRequest(dictionary, 'N')
-	def LoadVerbs():
-		return _SeparateAndRequest(dictionary, 'V')
-	def LoadCuss():
-		return _SeparateAndRequest(dictionary, 'C')
+			self.dictionary = dfile.read()
+	def LoadNouns(self):
+		return _SeparateAndRequest(self.dictionary, 'N')
+	def LoadVerbs(self):
+		return _SeparateAndRequest(self.dictionary, 'V')
+	def LoadCuss(self):
+		return _SeparateAndRequest(self.dictionary, 'C')
