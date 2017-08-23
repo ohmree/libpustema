e9e6a49aafca52ab736855f0787ac9a32a793073
Ahmed <adhoms@adhoms.public.domain.not.really>
2017-08-23 17:57:53 +0300

    aaaa

--- dicthandler.py
+++ dicthandler.py
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+import random
+
+def _SeparateAndRequest(dictionary, char):
+=======
 from enumutils import Types 
 
 # TODO: Give this a good name that helps understand 
@@ -8,21 +13,56 @@ def _SeparateAndRequest(dictionary, which):
 	and a Types enum member that specifices which type of word to get
 	Returns a list of words from the dictionary that are of type `which`
 	"""
+>>>>>>> b315ca564b141bd0058d9fa0e310e93374d5e851
 	words = []
-	i     = 0
 	text  = dictionary.split('\n')
 
 	for l in range(0,len(text)):
 		line = text[l].split(' ')
+<<<<<<< HEAD
+		if line[0] == char:
+			words.append(line[1])
+=======
 		if line[0] == which:
 			words[i] = line[1]
 			i += 1
+>>>>>>> b315ca564b141bd0058d9fa0e310e93374d5e851
 
 	return words
+
+def MakeUp(letter, index, length=0):
+	if length == 0:
+		length = random.randint(index,index+random.randint(0,5))
+	word = ''
+
+	for i in range(0,length):
+		char = random.randint(97,122)
+		word = word[:i] + chr(char) + word[i+1:]
+	word = word[:index] + letter + word[index+1:]
+
+	return word
+
+def LookFor(length, dictionary):
+	compat = []
+	for w in range(0,len(dictionary)):
+		if len(dictionary[w]) == length:
+			compat.append(dictionary[w])
+	return compat
 
 class Dictionary:
 	def __init__(self, filename):
 		with open(filename, 'r') as dfile:
 			self.dictionary = dfile.read()
+<<<<<<< HEAD
+	def LoadLetter(self, letter):
+		return _SeparateAndRequest(self.dictionary, letter)
+	def LoadNouns(self):
+		return _SeparateAndRequest(self.dictionary, 'N')
+	def LoadVerbs(self):
+		return _SeparateAndRequest(self.dictionary, 'V')
+	def LoadCuss(self):
+		return _SeparateAndRequest(self.dictionary, 'C')
+=======
 	def LoadWords(self, which):
 		return _SeparateAndRequest(self.dictionary, which)
+>>>>>>> b315ca564b141bd0058d9fa0e310e93374d5e851
--- libpustema.py
+++ libpustema.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
+import random
 import dicthandler
 import random
 from enumutils import Methods, Types
 
 filename = "dict.txt"
-string   = "da"
+string   = "zotzoot"
 song     = ""
 
 dictionary = dicthandler.Dictionary(filename)
+<<<<<<< HEAD
+=======
 method = random.randint(0, 3)
+>>>>>>> b315ca564b141bd0058d9fa0e310e93374d5e851
 length     = len(string)
 
 for i in range(0, length):
-	word1 = ""
-	word2 = ""
+	word1 = ''
+	word2 = ''
 
+<<<<<<< HEAD
+	method = 0 #method     = random.randint(0,3)
+	if   method == 0:
+		dict1 = dictionary.LoadNouns()
+		dict2 = dictionary.LoadNouns()
+	elif method == 1:
+		dict1 = dictionary.LoadNouns()
+		dict2 = dictionary.LoadVerbs()
+	elif method == 2:
+		dict1 = dictionary.LoadVerbs()
+		dict2 = dictionary.LoadVerbs()
+	elif method == 3:
+		dict1 = dictionary.LoadNouns()
+		dict2 = dictionary.LoadCuss()
+=======
 	if   method == Methods.NN:
 		dict1 = dictionary.LoadWords(Types.NOUN)
 		dict2 = dictionary.LoadWords(Types.NOUN)
@@ -30,25 +48,46 @@ for i in range(0, length):
 	elif method == Methods.NC:
 		dict1 = dictionary.LoadWords(Types.NOUN)
 		dict2 = dictionary.LoadWords(Types.CUSS)
+>>>>>>> b315ca564b141bd0058d9fa0e310e93374d5e851
 
-	len1 = len(dict1)
-	len2 = len(dict2)
+	lend1  = len(dict1)
+	lend2  = len(dict2)
+	shortl = len(dict1[0])
+	shortw = dict1[0]
+	longl  = len(dict1[0])
+	longw  = dict1[0]
+	#word2 will be treated as if word1+space is its prefix
+	for w in range(0, lend1):
+		if i < len(dict1[w]):
+			if dict1[w][i] == string[i]:
+				word1 = dict1[w]
+				word2 = dict2[random.randint(0,lend2-1)]
+			if len(dict1[w]) < shortl:
+				shortl = len(dict1[w])
+				shortw = dict1[w]
+			if len(dict1[w]) > longl:
+				longl = len(dict1[w])
+				longw = dict1[w]
 
-	for w in range(0, len1):
-		if dict1[w][i] == string[i]:
-			word1 = dict1[w]
-			w = len1
-	if word1 == "":
-		word1 = dict1[random.randint(0,len1)]
-		for w in range(0, len2):
-			if dict2[w][i] == string[i-len1-1]:
+	if word1 == '':
+		for w in range(0, lend2):
+			if string[i] in dict2[w]:
+				for x in range(0, len(dict2[w])):
+					if dict2[w][x] == string[i]:
+						position = x
+						x = len(dict2[w])
+				#word1 = dicthandler.LookFor(i-position, dict1)[random.randint(0,len(dicthandler.LookFor(i-position,dict1))-1)]
+				#print(i-position+1)
+				word1 = dicthandler.LookFor(i-position+1, dict1)[0]
 				word2 = dict2[w]
-	else:
-		word2 = dict2[random.randint(0,len2)]
+				w = lend2
+	if word1 == '':
+		word1 = dict1[random.randint(0,lend1-1)]
+		if i >= longl:
+			word2 = dicthandler.MakeUp(string[i], i-len(word1))
+		else:
+			word1 = dicthandler.MakeUp(string[i], i, longl)
+			word2 = dict2[random.randint(0,lend2-1)]
 
-	song += word1
-	song += " "
-	song += word2
-	song += "\n"
-
-print(song)
+	#print(word1 + ' ' + word2)
+	print(word1 + " " + word2)
